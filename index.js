c641a5ddb9b3a7eacfe8ab16c1a8b261b0b8fcb0
Christopher Lai <christopherliamlai@me.com>
2018-05-08 08:32:58 -0700

    Fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var neat = require("bourbon-neat");
 var includePaths = [];
 
 includePaths.concat(path.join(__dirname, "core"));
-includePaths.contact(bourbon.includePaths);
+includePaths.contat(bourbon.includePaths);
 includePaths.concat(neat.includePaths);
 
 module.exports = {
